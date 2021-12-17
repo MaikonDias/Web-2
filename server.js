cc6cfc8337f1a8ded4ff5a6a7395b2a9a3bd6c6c
Maikon Alexander de Barros Dias <maikonalexander.mabd@gmail.com>
2021-12-17 19:45:23 -0300

    Updates no server.js
    server.js atualizado para remodelar o uso do mecanismo de template

--- server.js
+++ server.js
@@ -3,15 +3,27 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-var hbs = require('express-handlebars');
+const mongoose = require('mongoose');
+var exphbs = require('express-handlebars');
 const routes = require('./src/routes');
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
-
-app.set('view engine', 'hbs');
+// mongoose.connect('mongodb://localhost:27017', {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+// }, function(err){
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log("MongoDB conectado")
+//     }
+// })
+app.engine('.hbs', exphbs.engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 app.set('views',path.join(__dirname, 'view'));
 
 app.use(cors());
